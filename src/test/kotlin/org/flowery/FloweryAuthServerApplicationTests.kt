fd72eb68c190ff4f15473dd2e71e3594ec9a2838
poqo <zxvm5962@naver.com>
2024-12-28 15:21:45 +0900

    initial commit

---
+++ src/test/kotlin/org/flowery/FloweryAuthServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.flowery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FloweryAuthServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
