7aeeff480a7c4a779a2516d051d5c7626292720c
poqo <zxvm5962@naver.com>
2025-01-19 17:35:37 +0900

    feat: Add sign-up endpoint with password validation

--- src/main/kotlin/org/flowery/service/AuthService.kt
+++ src/main/kotlin/org/flowery/service/AuthService.kt
@@ -1,13 +1,11 @@
 package org.flowery.service
 
-import org.flowery.dto.EmailSendDto
-import org.flowery.dto.EmailVerificationDto
-import org.flowery.dto.LoginRequestDto
-import org.flowery.dto.LoginResponseDto
+import org.flowery.dto.*
 import reactor.core.publisher.Mono
 
 interface AuthService {
     fun login(loginRequestDto: LoginRequestDto): Mono<LoginResponseDto>
     fun sendEmailMessage(data: EmailSendDto): String
     fun verificationEmail(data: EmailVerificationDto): String
+    fun signUp(signUpRequestDto: SignUpRequestDto): Mono<SignUpResponseDto>
 }
